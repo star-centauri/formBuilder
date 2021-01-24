2b50ebdd4177fd94f300687e77a9f4a19be17bf1
TRIAD-DEV <suporte@triad.inf.br>
2021-01-24 15:04:04 -0300

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
+import $ from "jquery";
 import './App.css';
 
+window.jQuery = $;
+window.$ = $;
+
+require("jquery-ui-sortable");
+require("formBuilder");
+
+
 function App() {
+  const form = useRef(null);
+
+  useEffect(() => {
+    $(form.current).formBuilder();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="fb-builder" ref={form}></div>
     </div>
   );
 }
